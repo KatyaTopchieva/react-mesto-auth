cbb65673ea31949ba249f01999601358d4c9c703
Ekaterina Topchieva <ekaterinatop4ieva@yandex.ru>
2022-06-29 19:09:06 +0300

    "feat: стилизовать подсказку"

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -13,13 +13,11 @@ class PopupWithForm extends React.Component {
          <button className={`popup__close-button popup__close-button_${this.props.name}`} type="button" onClick={this.props.onClose}></button>
           <form className={`popup__form popup__form_${this.props.name}`} name={this.props.name} >
             <h2 className={`popup__form-title popup__form-title_${this.props.name}`}>{this.props.title}</h2>
-            <fieldset className="popup__fieldset">
+            <fieldset className={`popup__fieldset popup__fieldset_${this.props.name}`}>
               {this.props.children}
-             
                 <button className={`popup__button popup__button_${this.props.name}`} type="submit" onClick={this.props.submit}>
                   {this.props.buttonText}
                 </button>
-            
             </fieldset>
           </form>
         </div>
